31f6f62ef8d821f3054edf36e654ab4df8d18f3b
Huse_wm72@itstep.edu.az <Huse_wm72@itstep.edu.az>
2022-11-30 18:35:56 +0400

    last Commit

---
+++ hw_3/Facade.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+class Stock
+{
+	public string CheckStock(string orderId)
+    {
+		return (orderId != "") ? "Available" : "Not Available";
+    }
+}
+
+class Card
+{
+	public string AddOrder(string orderId)
+    {
+		return "Order Added Successfully";
+    }
+
+	public string RemoveOrder(string orderId)
+    {
+		return "Order Removed Successfully";
+    }
+
+	public string CheckOutOrder(string orderId)
+    {
+		return "Order Checkout Successfully";
+    }
+}
+
+class Payment
+{
+	public string CheckPayment(string paymentDetails)
+    {
+		return (paymentDetails != "") ? "Valid Payment" : "Not Valid Payment";
+    }
+
+	public string DeductPayment(string orderId)
+    {
+		return (orderId != "") ? "Payment Deducted Successfully" : "Payment Not Being. Check From Your Order";
+    }
+}
+
+class OrderFacade
+{
+	private Stock stock;
+	private Card card;
+	private Payment payment;
+
+	public OrderFacade()
+	{
+		stock = new Stock();
+		card = new Card();
+		payment = new Payment();
+	}
+
+	public string CheckStockAvailability(string orderId)
+	{
+		string checkStockAvailability = stock.CheckStock(orderId);
+
+		return checkStockAvailability;
+	}
+
+	public string AddToCard(string orderId) => card.AddOrder(orderId);
+
+	public string RemoveFromCard(string orderId) => card.RemoveOrder(orderId);
+
+	public string CheckoutOrderFromCard(string orderId) => card.CheckOutOrder(orderId);
+	
+	public void PlaceOrder(string orderId)
+	{
+		string paymentValidationStatus = payment.CheckPayment(orderId);
+		Console.WriteLine(string.Format("\nPayment Validation Status: {0}", paymentValidationStatus));
+		string deductPaymentStatus = payment.DeductPayment(orderId);
+		Console.WriteLine(string.Format("\nPayment Deduct Status: {0}", deductPaymentStatus));
+	}
+}
+
+class Program
+{
+	static void Main()
+	{
+		OrderFacade order = new OrderFacade();
+		string orderId = "order 1";
+		string orderStatus = order.CheckStockAvailability(orderId);
+
+		if (orderStatus == "Available")
+		{
+			Console.WriteLine(string.Format("\nStock Status: {0}", orderStatus));
+			Console.WriteLine(string.Format("\nAdd Card Status: {0}", order.AddToCard(orderId)));
+			Console.WriteLine(string.Format("\nCheckout Order From Card Status: {0}", order.CheckoutOrderFromCard(orderId)));
+			order.PlaceOrder(orderId);
+		}
+		else
+        {
+			Console.WriteLine(string.Format("\nStock Status: {0}", orderStatus));
+        }
+	}
+}
---
+++ hw_3_2/Program.cs
@@ -0,0 +1,138 @@
+﻿using System.Text;
+
+interface IDataSource
+{
+	public string? Filename { get; set; }
+	void WriteData(string data);
+	string ReadData();
+}
+
+class FileDataSource : IDataSource
+{
+	public string? Filename { get; set; }
+	public FileDataSource(string path)
+    {
+		Filename = path;
+    }
+
+	public void WriteData(string data)
+	{
+		using FileStream fs = new FileStream(Filename, FileMode.Open);
+
+		using StreamWriter sw = new StreamWriter(fs);
+		sw.Write(data);
+	}
+
+	public string ReadData()
+	{
+		using StreamReader sr = new StreamReader(Filename);
+		string data = sr.ReadToEnd();
+
+		return data;
+	}
+}
+
+class Application
+{
+	private readonly IDataSource _datasource;
+
+	public Application(IDataSource datasource)
+    {
+		 _datasource = datasource;
+    }
+
+	public void WriteData(string data)
+    {
+		 _datasource.WriteData(data);
+    }
+
+	public string ReadData(string path)
+    {
+		 return _datasource.ReadData();
+    }
+}
+
+abstract class DataSourceDecorator : IDataSource
+{
+	protected IDataSource _wrappe { get; set; }
+	public string? Filename { get; set; }
+	public DataSourceDecorator(IDataSource datasource, string file)
+	{
+		_wrappe = datasource;
+		Filename = file;
+	}
+
+	public virtual void WriteData(string message)
+	{
+	}
+
+	public virtual string ReadData()
+	{
+
+		return "";
+	}
+}
+
+
+class EncryptionDecorator : DataSourceDecorator
+{
+	char key = 'M';
+	public EncryptionDecorator(string FileName, IDataSource? datasource = null) : base(datasource, FileName) { }
+
+	public override string ReadData()
+	{
+		using StreamReader sr = new StreamReader(Filename);
+		string data = sr.ReadToEnd();
+		var decrypted = new StringBuilder();
+		string encr = "";
+
+		for (int i = 0; i < data.Length; i++)
+        {
+			encr = encr + char.ToString((char)(data[i] ^ key));
+
+        }
+
+		return encr.ToString();
+	}
+
+	public override void WriteData(string data)
+	{
+		int len = data.Length;
+		string encr = "";
+		for (int i = 0; i < len; i++)
+        {
+			encr = encr + char.ToString((char)(data[i] ^ key));
+        }
+
+		using FileStream fs = new FileStream(Filename, FileMode.OpenOrCreate);
+		using StreamWriter sw = new StreamWriter(fs);
+		sw.Write(encr);
+	}
+}
+
+class CompressionDecorator : DataSourceDecorator
+{
+	public CompressionDecorator(IDataSource datasource, string FileName) : base(datasource, FileName) { }
+
+	public override string ReadData()
+	{
+		return "";
+	}
+
+	public override void WriteData(string message)
+	{
+	}
+}
+
+class Program
+{
+	static void Main()
+	{
+		string file = "myfile.txt";
+		string path = @$"C:\Users\{Environment.UserName}\Desktop\{file}";
+		IDataSource data = new FileDataSource(path);
+		data = new EncryptionDecorator(path);
+		data.WriteData("CRISTIANO RONALDO");
+		Console.WriteLine(data.ReadData());
+	}
+}
